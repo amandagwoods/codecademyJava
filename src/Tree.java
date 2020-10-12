9aaad08581957e58816f36b43edf81a2b3a09ad2
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-12 12:48:31 -0500

    feat: plant a tree exercise

---
+++ src/Tree.java
@@ -0,0 +1,17 @@
+// Define your tree class in this file!
+public class Tree{
+    public static void main(String[] args){
+        //introducing ourself and printing tree
+
+        System.out.println("Hello, I'm Amanda I'm practing Java!");
+
+        System.out.println("I'm goign to plant a tree right here on screen! Watch!");
+
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+    }
+}
