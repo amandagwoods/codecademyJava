fb4b05e907950f9f088e06b498a3ffc73aaeb485
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-11-09 23:31:01 -0600

    feat: working on magic math project

--- src/MathMagic.java
+++ src/MathMagic.java
@@ -1,2 +1,37 @@
 public class MathMagic {
+
+    public void calculator(){
+    }
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int subtract(int a, int b){
+        return a - b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a, int b){
+        return a / b;
+    }
+
+    public int modulo(int a, int b){
+        return a % b;
+    }
+
+
+    public static void main(String[] agrs){
+        Calculator myCalculator = new Calculator();
+        int addition = myCalculator.add(5, 7);
+        System.out.println(addition);
+
+        int subtraction = myCalculator.subtract(45, 11);
+        System.out.println(subtraction);
+
+
+    }
 }
